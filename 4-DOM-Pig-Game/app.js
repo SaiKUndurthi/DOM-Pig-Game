fa7e7daad197e934fa9b65f1227a15e9dd271d24
SaiKUndurthi <sai.undurthi@gmail.com>
2018-01-16 23:19:30 -0800

    Created a Game initialization function.

--- 4-DOM-Pig-Game/app.js
+++ 4-DOM-Pig-Game/app.js
@@ -10,20 +10,9 @@ GAME RULES:
 */
 var scores, roundScore, activePlayer;
 
-scores = [0,0];
-roundScore = 0;
-activePlayer = 0;
 
+init();
 
-//document.querySelector('#current-'+activePlayer).textContent = dice;
-//var x = document.querySelector('#score-0').textContent;
-//document.
-document.querySelector('.dice').style.display = 'none';
-
-document.getElementById('current-0').textContent = '0';
-document.getElementById('current-1').textContent = '0';
-document.getElementById('score-0').textContent = '0';
-document.getElementById('score-1').textContent = '0';
 
 document.querySelector('.btn-roll').addEventListener('click', function(){
   //random number
@@ -58,14 +47,39 @@ document.querySelector('.btn-hold').addEventListener('click', function(){
   }
 });
 
+document.querySelector('.btn-new').addEventListener('click', init);
+
+
+function init(){
+  scores = [0,0];
+  roundScore = 0;
+  activePlayer = 0;
+
+  //document.querySelector('#current-'+activePlayer).textContent = dice;
+  //var x = document.querySelector('#score-0').textContent;
+  //document.
+  document.querySelector('.dice').style.display = 'none';
+
+  document.getElementById('current-0').textContent = '0';
+  document.getElementById('current-1').textContent = '0';
+  document.getElementById('score-0').textContent = '0';
+  document.getElementById('score-1').textContent = '0';
+  document.getElementById('name-0').textContent = 'Player 1';
+  document.getElementById('name-1').textContent = 'Player 2';
+  document.querySelector('.player-0-panel').classList.remove('winner');
+  document.querySelector('.player-1-panel').classList.remove('winner');
+  document.querySelector('.player-0-panel').classList.remove('active');
+  document.querySelector('.player-1-panel').classList.remove('active');
+  document.querySelector('.player-0-panel').classList.add('active');
+
+}
+
 function nextPlayer(){
   activePlayer === 1 ? activePlayer = 0 : activePlayer = 1;
   roundScore = 0;
   document.getElementById('current-0').textContent = '0';
   document.getElementById('current-1').textContent = '0';
 
-  //document.querySelector('player-1-panel').classList.add('active');
-  //document.querySelector('player-1-panel').classList.remove('active');
   document.querySelector('.player-1-panel').classList.toggle('active');
   document.querySelector('.player-0-panel').classList.toggle('active');
 
